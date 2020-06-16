947709e3e4e1a09315a27ffe392423a8613adf77
Nicat Quliyev <optima.nicat@yahoo.com>
2020-06-16 14:05:14 +0400

    First commit

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class TasksController extends Controller
+{
+
+    public function storeTask(Request $request){
+
+        $this->validate($request, ['title' => 'required']);
+
+        $success = DB::table('tasks')->insert([
+            'title' => $request->title,
+            'isDone' => $request->has('isDone') ? $request->isDone : 0
+        ]);
+
+        if(!$success)
+        {
+            $response = [
+                'code' => 400,
+                'message' => 'Məlumatların daxil olunması zamanı xəta baş verdi.'
+            ];
+
+            return response(json_encode($response), 400);
+        }
+
+        $response = [
+            'code' => 201,
+            'message' => 'Məlumatlar uğurla yaradıldı.'
+        ];
+
+        return response(json_encode($response), 201);
+    }
+
+    public function getTasks(){
+        $tasks = DB::table('tasks')
+                            ->select()
+                            ->get();
+
+        if($tasks)
+        {
+            foreach($tasks as $task)
+            {
+                $task->isDone = $task->isDone == 0 ? false : true;
+            }
+        }
+
+        return response(json_encode($tasks), 200);
+    }
+
+    public function updateTask(Request $request)
+    {
+        $this->validate($request, [
+           'id' => 'required'
+        ]);
+
+        $success = DB::table('tasks')
+        ->where('id', '=', $request->id)
+        ->update([
+            'title' => $request->title,
+            'isDone' => $request->isDone
+        ]);
+
+        if(!$success)
+        {
+            $response = [
+                'code' => 400,
+                'message' => 'Məlumatların yenilənməsi zamanı xəta baş verdi.'
+            ];
+
+            return response(json_encode($response), 400);
+        }
+
+        $response = [
+            'code' => 200,
+            'message' => 'Məlumatlar uğurla yeniləndi.'
+        ];
+
+        return response(json_encode($response), 200);
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+use App\Http\Controllers\TasksController;
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+$router->group(['prefix' => 'api/v1'], function () use ($router) {
+    $router->post('tasks', 'TasksController@storeTask');
+    $router->get('tasks', 'TasksController@getTasks');
+    $router->put('tasks', 'TasksController@updateTask');
+});
